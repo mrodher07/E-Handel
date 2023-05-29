62d3c6260519d4eaba39fcbc00c8f414863d8a3f
mrodher07 <miguelroher2002@gmail.com>
2023-05-29 22:48:28 +0200

    Added the addNewProduct funcionality. It has left to get the getDownloadUrl

--- app/src/main/java/com/example/eHandel/adminActivities/AdminAddNewProductActivity.kt
+++ app/src/main/java/com/example/eHandel/adminActivities/AdminAddNewProductActivity.kt
@@ -1,17 +1,38 @@
 package com.example.eHandel.adminActivities
 
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
-import com.example.eHandel.MainActivity
+import androidx.appcompat.app.AlertDialog
+import com.example.eHandel.R
 import com.example.eHandel.databinding.ActivityAdminAddNewProductBinding
 import com.google.firebase.auth.FirebaseAuth
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
 
 class AdminAddNewProductActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAdminAddNewProductBinding
     private lateinit var auth: FirebaseAuth
+    val storage = FirebaseStorage.getInstance()
+    val firestore = FirebaseFirestore.getInstance()
     private var tipo = "";
+    private var imageUri: Uri? = null
+    private var imageByteArray: ByteArray? = null
+    var downloadUrl: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAdminAddNewProductBinding.inflate(layoutInflater)
@@ -19,16 +40,194 @@ class AdminAddNewProductActivity : AppCompatActivity() {
 
         recogerTipo()
 
+        binding.ivNewProductPhoto.setOnClickListener {
+            productImage()
+        }
+
+        binding.btnAddNewProduct.setOnClickListener {
+            var name = binding.etAddNewProductName.text.toString().trim()
+            var vendor = binding.etAddNewProductVendor.text.toString().trim()
+            var price = binding.etAddNewProductPrice.text.toString().toDouble()
+            var quantity = binding.etAddNewProductQuantity.text.toString().toInt()
+            addNewProduct(name, vendor, price, quantity)
+        }
+
         // Esto es provisional quitar cuando se empiece a crear la activity
         auth = FirebaseAuth.getInstance()
         var nombre = auth.currentUser?.email
-        Toast.makeText(this, "Ha entrado el ADMIN", Toast.LENGTH_SHORT).show()
-        auth.signOut()
-        startActivity(Intent(this, MainActivity::class.java))
+        //Toast.makeText(this, "Ha entrado el ADMIN", Toast.LENGTH_SHORT).show()
+        //auth.signOut()
+        //startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    companion object {
+        const val PICK_IMAGE_REQUEST_GALERIA=20
+        const val PICK_IMAGE_REQUEST_CAMERA=21
     }
 
     private fun recogerTipo(){
         val mibundle = intent.extras
         tipo = mibundle?.getString("type").toString()
+        binding.tvAddNewProductType.text = binding.tvAddNewProductType.text.toString().trim() + tipo
+
+    }
+
+    private fun productImage(){
+        var options = listOf(resources.getString(R.string.productImagePopupCamera), resources.getString(R.string.productImagePopupGallery))
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(resources.getString(R.string.productImagePopup))
+        builder.setItems(options.toTypedArray()){_, item ->
+            when{
+                options[item] == resources.getString(R.string.productImagePopupCamera)-> {
+                    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    startActivityForResult(intent, PICK_IMAGE_REQUEST_CAMERA)
+                }
+                options[item] == resources.getString(R.string.productImagePopupGallery)-> {
+                    val intent = Intent(Intent.ACTION_PICK)
+                    intent.type = "image/*"
+                    startActivityForResult(intent, PICK_IMAGE_REQUEST_GALERIA)
+                }
+            }
+        }
+        builder.show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == PICK_IMAGE_REQUEST_GALERIA && resultCode == Activity.RESULT_OK){
+            imageUri = data?.data
+            imageByteArray = null
+            binding.ivNewProductPhoto.setImageURI(imageUri)
+            binding.ivNewProductPhoto.setTag(imageUri)
+        } else if(requestCode == PICK_IMAGE_REQUEST_CAMERA && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageUri = null
+            imageByteArray = changeBitmap(applicationContext, imageBitmap)
+            binding.ivNewProductPhoto.setImageBitmap(imageBitmap)
+            binding.ivNewProductPhoto.setTag(imageBitmap)
+            binding.ivNewProductPhoto.scaleX = 1.0F
+            binding.ivNewProductPhoto.scaleY = 1.0F
+
+        }
+    }
+
+    private fun changeBitmap(context: Context, bitmap: Bitmap): ByteArray? {
+        var byteArrayOutputStream = ByteArrayOutputStream()
+        var byteArray : ByteArray? = null
+        try {
+
+            var img = bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+            byteArray = byteArrayOutputStream.toByteArray()
+
+            /*val file = File(context.filesDir, "Image_${System.currentTimeMillis()}.jpg")
+            fileOutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)
+            fileOutputStream.flush()
+            imageUri = Uri.fromFile(file)*/
+        }catch (e: IOException){
+            e.printStackTrace()
+        }finally {
+            try {
+               byteArrayOutputStream?.close()
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+        }
+        return byteArray
+    }
+
+    private fun uploadPhotoToFirebaseStorage(){
+        if(imageUri != null){
+            val storageRef = storage.reference.child("photos")
+            val imageRef = storageRef.child("imagen")
+            imageRef.putFile(imageUri!!).addOnSuccessListener {
+                imageRef.downloadUrl.addOnSuccessListener { uri ->
+                    downloadUrl = uri.toString()
+                }
+            }
+
+
+            /*uploadTask.continueWithTask{ task ->
+                if(!task.isSuccessful){
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                imageRef.downloadUrl
+            }.addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    downloadUrl = task.result.toString()
+                }else{
+
+                }
+            }*/
+        }else if (imageByteArray != null){
+            val storageRef = storage.reference.child("photos")
+            val imageRef = storageRef.child("imagenPrueba")
+            imageRef.putBytes(imageByteArray!!).addOnSuccessListener {
+                println("Ha entrado")
+                imageRef.downloadUrl.addOnSuccessListener { uri ->
+                    downloadUrl = uri.toString()
+                }
+            }.addOnFailureListener{
+                println("Ha dado fallo")
+            }.addOnCanceledListener {
+                println("Se ha cancelado")
+            }
+        }
+    }
+
+    private fun addNewProduct(name: String, vendor: String, price: Double, quantity: Number){
+
+        val firestoreDB = FirebaseFirestore.getInstance()
+        val db = Firebase.firestore
+        if(imageUri != null){
+            val storageRef = storage.reference.child("photos")
+            val imageRef = storageRef.child("imagen")
+            imageRef.putFile(imageUri!!).addOnSuccessListener {
+                imageRef.downloadUrl.addOnSuccessListener { uri ->
+                    downloadUrl = uri.toString()
+                }
+            }
+        }else if (imageByteArray != null){
+            val storageRef = storage.reference.child("photos")
+            val imageRef = storageRef.child("imagenPrueba")
+            imageRef.putBytes(imageByteArray!!).addOnSuccessListener {
+                println("Ha entrado")
+                imageRef.downloadUrl.addOnSuccessListener { uri ->
+                    downloadUrl = uri.toString()
+                }
+            }.addOnFailureListener{
+                println("Ha dado fallo")
+            }.addOnCanceledListener {
+                println("Se ha cancelado")
+            }
+        }
+
+        val data = hashMapOf(
+            "name" to name,
+            "type" to tipo,
+            "image" to downloadUrl,
+            "vendor" to vendor,
+            "price" to price,
+            "quantity" to quantity,
+        )
+
+        firestoreDB.collection("products")
+            .document(name)
+            .get()
+            .addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    val document = task.result
+                    if (document.exists()){
+                        Toast.makeText(this, "El producto creado ya existe", Toast.LENGTH_SHORT).show()
+                    }else{
+                        db.collection("products").document(name)
+                            .set(data)
+                            .addOnSuccessListener { Log.d(ContentValues.TAG, "DocumentSnapshot successfully written!")}
+                            .addOnFailureListener{ e -> println(e.message)}
+                    }
+                }
+            }
     }
 }
